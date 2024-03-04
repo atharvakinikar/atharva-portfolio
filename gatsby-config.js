0eeef967e04f3d797a565751900bc76336a17772
atharvakinikar <atharva.kinikar123@gmail.com>
2024-03-04 13:08:22 +0530

    updated resume

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
     title: 'Atharva Kinikar',
     description:
       'Atharva Kinikar is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+    siteUrl: 'https://atharva-kinikar-portfolio.netlify.app', // No trailing slash allowed!
     image: '/me_2.jpeg', // Path to your image you placed in the 'static' folder
     twitterUsername: '@bchiang7',
   },
